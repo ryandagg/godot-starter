9f787a4b688dfb4f1b6f7a3c3525dfa06271f0c8
RyanDagg <rdagg@salesforce.com>
2025-08-11 15:00:10 -0400

    test: add test file to trigger pre-commit hook

--- Scenes/Main/Version.gd
+++ Scenes/Main/Version.gd
@@ -3,5 +3,5 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# set test as number from project file
-	text = "Version: " + str(ProjectSettings.get_setting("application/config/version"))
+	# Display Hello World message
+	text = "Hello World!"
